e18cd5c2c295f86acc2a2c4dd04bce85f8ac9ca0
OFFICE <office@OFFICEs-MacBook-Air.local>
2019-07-11 21:20:42 -0400

    writing test for the first api call

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ ruby '2.3.7'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'figaro'
 gem 'bootstrap-sass', '3.3.7'
+gem 'httparty'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
---
+++ spec/controllers/weather_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WeatherController, type: :controller do
+  describe 'GET #' do
+
+    it 'returns a response of 200' do
+      get("https://api.openweathermap.org/data/2.5/weather?q=ottawa&units=metric&APPID=#{ENV['weatherbit_api']}")
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
